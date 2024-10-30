07f26339ed0c80d611b96899b2f0e075fba5ba09
Besco <bernardbolter@gmail.com>
2024-10-30 17:23:36 +0100

    updated about wav page with about image and link

--- src/components/AboutWAV.jsx
+++ src/components/AboutWAV.jsx
@@ -1,6 +1,8 @@
 import { useContext } from "react"
 import { DemonContext } from "@/providers/DemonProvider"
 
+import Image from "next/image"
+
 import Close from "@/svg/Close"
 
 const AboutWAV = () => {
@@ -16,13 +18,22 @@ const AboutWAV = () => {
             </div>
             <div className="about-wav-content-container">
                 <div className="about-wav-demon-container">
-
+                    <a 
+                        href="https://www.demonfootwear.com"
+                        className="about-nav-image-container"
+                    >
+                        <img src="/images/about/about.png" />
+                        <p className="about-nav-link">ENTER DEMONFOOTWEAR.COM</p>
+                    </a>
+                    <div className="about-wav-text-container">
+                        <p>Demon WAV hatched from the sheer vocation of Alberto Deon to make music. Semantically, WAV is a direct reference to the audio file format known for best preserving the quality of sounds. The expression WAV stands as an etiological allegory to the universe of computer music, and the way it impacted a generational taste.</p>
+                        <p>In Demon WAV, music becomes the chosen channel for interweaving cultural and autobiographical references. Expanding the idea of brand, and product into a multifaceted reality that ultimately becomes a mirror to the self.</p>
+                    </div>
                 </div>
                 <div className="about-wav-product-container">
                     
                 </div>
             </div>
-            <h1>ABOUT WAV</h1>
         </section>
     )
 }
